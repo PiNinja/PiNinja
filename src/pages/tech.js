2edcf17478f87cbf32ff19ae4d9f2df719afcc53
PookMook <arthur.juchereau@gmail.com>
2019-02-08 17:29:20 -0500

    add tech

--- src/pages/tech.js
+++ src/pages/tech.js
@@ -1,4 +1,45 @@
 import React from "react"
 import Layout from '../layouts/layout'
 
-export default () => <Layout></Layout>
+export default () => (
+    <Layout>
+        <main className="multiple">
+            <article>
+                <h1><strong>React</strong> - 90%</h1>
+                <ul className="sub">
+                    <li className="ok">Hooks</li>
+                    <li className="ko">tests</li>
+                </ul>
+            </article>
+            <article>
+                <h1><strong>GraphQL</strong> - 50%</h1>
+                <ul className="sub">
+                    <li className="ok">Schema</li>
+                    <li className="ok">Custom rules</li>
+                    <li className="ok">Authentification</li>
+                    <li className="ko">tests</li>
+                </ul>
+            </article>
+            <article>
+                <h1><strong>A-frame</strong> - 25%</h1>
+                <ul className="sub">
+                    <li className="ok">basics</li>
+                    <li className="ko">Import custom model</li>
+                    <li className="ko">fetch API data</li>
+                    <li className="ko">Interractions</li>
+                    <li className="ko">Multiplayer</li>
+                </ul>
+            </article>
+            <article>
+                <h1><strong>Parser</strong> - 15%</h1>
+                <ul className="sub">
+                    <li className="ok">Schema</li>
+                    <li className="ko">Custom functions</li>
+                    <li className="ko">NPM package</li>
+                    <li className="ko">tests</li>
+
+                </ul>
+            </article>
+        </main>
+    </Layout>
+)
