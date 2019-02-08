8d1d33288fd6bfe3bf2a654a54124b18c4d71741
PookMook <arthur.juchereau@gmail.com>
2019-02-08 11:03:24 -0500

    better css handling

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Layout from './../layouts/layout'
 export default () => (
     <Layout>
         <h1>Welcome to the <strong>Pi.Ninja</strong></h1>
-        <p>The <strong>Pi.Ninja</strong> is a <strong>Digital Workshop</strong> of Web Technologies. Our mission is two-fold in order to help <strong>invent the futur of the Web</strong></p>
-        <h2>Explore <strong>new technologies</strong>, test the wider range of <strong>opportunities</strong> they offer and their <strong>limitations</strong></h2>
-        <h2>Deliver <strong>small and efficient</strong> Applications or Libraries to <strong>solve real-world problems</strong></h2>
+        <p>The <strong>Pi.Ninja</strong> is a <strong>Digital Workshop</strong> of Web Technologies.<br/> Our mission is two-fold in order to help <strong>invent the futur of the Web</strong></p>
+        <h2>Explore <strong>new technologies</strong>,<br/> test the wider range of <strong>opportunities</strong> they offer and their <strong>limitations</strong></h2>
+        <h2>Deliver <strong>small and efficient</strong> Applications or Libraries<br/> to <strong>solve real-world problems</strong></h2>
         <p>We specialize in <strong>highly custom</strong> Web platforms and <strong>open-source libraries</strong>.</p>
     </Layout>
 )
