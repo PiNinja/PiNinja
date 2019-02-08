cf7b66ec24afe158fbbcb02b083f9919ee4921cc
PookMook <arthur.juchereau@gmail.com>
2019-02-08 12:38:39 -0500

    fix finally using gatsby-ism

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -8,25 +8,20 @@ import "../../styles/background.scss"
 
 
 const Header = () => {
+    const isBrowser = typeof window !== 'undefined';
     const loadedVisualization = loadVizualization();
-    console.log(loadedVisualization)
     const [expand,setExpand] = useState(false)
-    let [visualization, setVisualization] =  useState(loadedVisualization)
-
-    useEffect(()=>{
-        console.log("moving to ",loadVizualization())
-        setVisualization(loadVizualization())
-    }, {})
+    const [visualization, setVisualization] =  useState(loadedVisualization)
 
     const mainDigits = ['3.',1,4,1,5,9]
     
     return (
         <>
-            <section id="background" className={loadVizualization()}></section>
+            {isBrowser && <section id="background" className={visualization}></section>}
             <header>
-                <section className={visualization}>
+                {isBrowser && <section className={visualization}>
                 {mainDigits.map((d,i)=>(<div key={'MainDigit'+i} onClick={()=>setVisualization(cycleVisualization(visualization))}><p className={`digit${parseInt(d)}`}>{d}</p></div>))}
-                </section>
+                </section>}
                 <div id="burger" onClick={()=>setExpand(!expand)}>=</div>
                 <nav>
                     <ul>
