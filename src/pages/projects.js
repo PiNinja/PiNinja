ac94e7e399e38da27311f94b2e4da8ee41de34d2
PookMook <arthur.juchereau@gmail.com>
2019-05-06 22:56:21 -0400

    add Stylo

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -16,6 +16,18 @@ export default () => (
                 </ul>
             </article>
             <article>
+                <h1><strong>Stylo</strong> - <a href="stylo.ecrituresnumeriques.ca" target="_blank" rel="noopener noreferrer"></a></h1>
+                <p>Blabla description de stylo</p>
+                <ul>
+                    <li>Mongodb</li>
+                    <li>GraphQL</li>
+                    <li>React/Redux</li>
+                    <li>Gatsby</li>
+                    <li>Inputs : MD / YaML / Bib</li>
+                    <li>Outputs : PDF / HTML5 / WORD / WORDX / XML / TEI / LATEX / ODT / EPUB / ICML</li>
+                </ul>
+            </article>
+            <article>
                 <h1><strong>JSONGraph</strong> - Undisclosed</h1>
                 <ul className="technologies">
                     <li className="lot">Parser</li>
